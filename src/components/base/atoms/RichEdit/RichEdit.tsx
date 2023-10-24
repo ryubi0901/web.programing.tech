d5a8608e4c807249a7797ddac33f83d81571c833
ryu-bi <0901baske@gmail.com>
2023-10-24 23:36:48 +0900

    スタイルを修正

--- src/components/base/atoms/RichEdit/RichEdit.tsx
+++ src/components/base/atoms/RichEdit/RichEdit.tsx
@@ -1,12 +1,14 @@
 import clsx from 'clsx'
 
+import styles from './RichEdit.module.css'
+
 type Props = {
   body: string
   className?: string
 }
 
 const RichEdit = ({ body, className }: Props) => {
-  return <div className={clsx(className)} dangerouslySetInnerHTML={{ __html: body }} />
+  return <div className={clsx(styles.RichEdit, className)} dangerouslySetInnerHTML={{ __html: body }} />
 }
 
 export default RichEdit
