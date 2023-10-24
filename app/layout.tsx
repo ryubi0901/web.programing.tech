a368ff0ba89256f1e8a2d108921d4d0f38871b6c
ryu-bi <0901baske@gmail.com>
2023-10-24 23:35:31 +0900

    フォント、スタイルの設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import '~/src/styles/index.css'
 import 'scroll-behavior-polyfill'
 import 'focus-visible'
+import { Metadata } from 'next'
+import { Noto_Serif_JP, Lato } from 'next/font/google'
 import { ReactNode, Suspense } from 'react'
 
 import { PageBetweenFocus } from '~/src/components/base/molecules/PageBetweenFocus'
@@ -8,10 +10,42 @@ import { PageBetweenFocus } from '~/src/components/base/molecules/PageBetweenFoc
 import { WindowNarrow } from '~/src/components/base/molecules/WindowNarrow'
 import { Layout } from '~/src/components/layout/Layout'
 
+const notoSerifJp = Noto_Serif_JP({
+  weight: ['400', '700'],
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-noto',
+})
+
+const lato = Lato({
+  weight: '400',
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-lato',
+})
+
+export const metadata: Metadata = {
+  //metadataBase: new URL(`${process.env.NEXT_PUBLIC_SITE_URL}${process.env.NEXT_PUBLIC_BASE_PATH}`),
+  title: {
+    default: 'Webプログラミング資料',
+    template: '%s | Webプログラミング資料',
+  },
+  description: '',
+  openGraph: {
+    title: '',
+    description: '',
+    images: '/og.png',
+  },
+  twitter: {
+    card: 'summary_large_image',
+  },
+  icons: [{ rel: 'icon', url: '/favicon.png' }],
+}
+
 export default function BaseLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ja">
-      <body className="font-sans leading-normal text-black">
+      <body className={`${notoSerifJp.variable} ${lato.variable} font-sans leading-loose tracking-3xl text-black`}>
         <Layout>{children}</Layout>
 
         <WindowNarrow />
