13991f92a1677d989bb48a4741d0232e65d33bb4
ryu-bi <0901baske@gmail.com>
2023-10-24 23:35:55 +0900

    CMSのクライアントを設定

--- src/utils/cms/apiClient.ts
+++ src/utils/cms/apiClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from 'microcms-js-sdk'
 
 export const apiClient = createClient({
-  serviceDomain: '',
-  apiKey: '',
+  serviceDomain: 'web-programing',
+  apiKey: 'QSyEArVT6TEVhGkc6O5dsBkREDIUR6yWBDLU',
 })
