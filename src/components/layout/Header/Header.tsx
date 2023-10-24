918623302b8aede44dddda4626c683833a9ebd01
ryu-bi <0901baske@gmail.com>
2023-10-24 23:36:37 +0900

    ヘッダーを作成

---
+++ src/components/layout/Header/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="bg-green-500 text-white py-10">
+      <div className="mx-auto w-11/12">
+        <h1 className="text-3xl">Webプログラミング</h1>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- src/components/layout/Layout/Layout.tsx
+++ src/components/layout/Layout/Layout.tsx
@@ -1,14 +1,18 @@
 import { ReactNode } from 'react'
 
+import { Header } from '../Header'
+
 export type Props = {
   children: ReactNode
 }
 
 const Layout = ({ children }: Props) => (
-  // {/* <Header /> */}
-  <main id="main" tabIndex={-1}>
-    {children}
-  </main>
+  <>
+    <Header />
+    <main id="main" tabIndex={-1}>
+      {children}
+    </main>
+  </>
   // {/* <Footer /> */}
 )
 
